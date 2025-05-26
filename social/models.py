f5996643e9a40926b865308af35dfb1bbb5a9de2
JamesMutahi <mutahijames0@gmail.com>
2025-05-26 21:11:04 +0300

    add post serrializer fields

--- social/models.py
+++ social/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 User = get_user_model()
 
@@ -36,14 +38,27 @@ class Post(BaseModel):
     video2 = models.ImageField(upload_to='posts/videos/', null=True, blank=True)
     video3 = models.ImageField(upload_to='posts/videos/', null=True, blank=True)
     reply_to = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='replies')
+    repost_of = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='reposts')
     likes = models.ManyToManyField(User, blank=True, related_name='post_likes')
     ip_address = models.GenericIPAddressField()
-    edited = models.BooleanField(default=False)
-    deleted = models.BooleanField(default=False)
-    active = models.BooleanField(default=True)
+    is_edited = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     ip_views = models.ManyToManyField(IpAddress, blank=True)
     user_views = models.ManyToManyField(User, blank=True)
 
     class Meta:
         ordering = ['-id']
         db_table = 'Post'
+
+    def __str__(self):
+        return self.body
+
+    def clean(self):
+        if self.reply_to is not None and self.repost_of is not None:
+            raise ValidationError(
+                {
+                    "reply_to": ValidationError(_("reply to and repost of cannot be selected at the same time"), code="invalid"),
+                }
+            )
+        
--- social/serializers.py
+++ social/serializers.py
@@ -1,9 +1,67 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from social.models import Post
 
+User = get_user_model()
+
+
+class SafeUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'first_name', 'last_name', 'is_verified', 'is_staff', 'is_active',)
+
 
 class PostSerializer(serializers.ModelSerializer):
+    author = SafeUserSerializer(read_only=True)
+    likes = serializers.SerializerMethodField()
+    replies = serializers.SerializerMethodField()
+    views = serializers.SerializerMethodField()
+
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = (
+            'id',
+            'author',
+            'created_at',
+            'updated_at',
+            'body',
+            'image1',
+            'image2',
+            'image3',
+            'image4',
+            'image5',
+            'image6',
+            'video1',
+            'video2',
+            'video3',
+            'is_edited',
+            'is_deleted',
+            'is_active',
+            'likes',
+            'views',
+            'reply_to',
+            'repost_of',
+            'replies',
+        )
+
+    def get_fields(self):
+        fields = super(PostSerializer, self).get_fields()
+        fields['reply_to'] = PostSerializer(read_only=True)
+        fields['repost_of'] = PostSerializer(read_only=True)
+        return fields
+
+    @staticmethod
+    def get_likes(obj):
+        count = obj.likes.count()
+        return count
+
+    @staticmethod
+    def get_replies(obj):
+        count = obj.replies.count() + obj.reposts.count()
+        return count
+
+    @staticmethod
+    def get_views(obj):
+        count = obj.ip_views.count()
+        return count
