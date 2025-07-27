4a708471675d759ff2001f223281fd963e5e515c
JamesMutahi <mutahijames0@gmail.com>
2025-07-27 14:10:26 +0300

    paginate surveys and add subscriptions per page

--- survey/consumers.py
+++ survey/consumers.py
@@ -4,6 +4,7 @@ from djangochannelsrestframework.mixins import ListModelMixin
 from djangochannelsrestframework.observer import model_observer
 from djangochannelsrestframework.observer.generics import ObserverModelInstanceMixin, action
 
+from chat.utils.list_paginator import list_paginator
 from survey.models import Survey, Question, Choice
 from survey.serializers import SurveySerializer, ResponseSerializer
 
@@ -19,28 +20,32 @@ class SurveyConsumer(ListModelMixin, ObserverModelInstanceMixin, GenericAsyncAPI
         else:
             await self.close()
 
-    async def accept(self, **kwargs):
-        await super().accept(**kwargs)
-        await self.survey_activity.subscribe()
-        await self.question_activity.subscribe()
-        await self.choice_activity.subscribe()
-
     @model_observer(Survey)
     async def survey_activity(self, message, observer=None, action=None, **kwargs):
+        instance = message.pop('data')
         if message['action'] != 'delete':
-            message['data'] = await self.get_survey_serializer_data(pk=message['pk'])
+            message['data'] = await self.get_survey_serializer_data(survey=instance)
         await self.send_json(message)
 
     @database_sync_to_async
-    def get_survey_serializer_data(self, pk):
-        survey = Survey.objects.get(pk=pk)
+    def get_survey_serializer_data(self, survey: Survey):
         serializer = SurveySerializer(instance=survey, context={'scope': self.scope})
         return serializer.data
+
+    @survey_activity.groups_for_signal
+    def survey_activity(self, instance: Survey, **kwargs):
+        yield f'survey__{instance.pk}'
+
+    @survey_activity.groups_for_consumer
+    def survey_activity(self, pk=None, **kwargs):
+        if pk is not None:
+            yield f'survey__{pk}'
 
     @survey_activity.serializer
     def survey_activity(self, instance: Survey, action, **kwargs):
         return dict(
             # data is overridden in model_observer
+            data=instance,
             action=action.value,
             request_id='surveys',
             pk=instance.pk,
@@ -49,24 +54,24 @@ class SurveyConsumer(ListModelMixin, ObserverModelInstanceMixin, GenericAsyncAPI
 
     @model_observer(Question)
     async def question_activity(self, message, observer=None, action=None, **kwargs):
-        data = await self.get_question_activity_data(pk=message['pk'])
-        if data is not None:
-            message['data'] = data
-            await self.send_json(message)
+        instance = message.pop('data')
+        message['data'] = await self.get_survey_serializer_data(survey=instance)
+        await self.send_json(message)
 
-    @database_sync_to_async
-    def get_question_activity_data(self, pk):
-        question_qs = Question.objects.filter(pk=pk)
-        if not question_qs.exists():
-            return None
-        survey = question_qs.first().survey
-        serializer = SurveySerializer(instance=survey, context={'scope': self.scope})
-        return serializer.data
+    @question_activity.groups_for_signal
+    def question_activity(self, instance: Question, **kwargs):
+        yield f'survey__{instance.survey.id}'
+
+    @question_activity.groups_for_consumer
+    def question_activity(self, survey=None, **kwargs):
+        if survey is not None:
+            yield f'survey__{survey}'
 
     @question_activity.serializer
     def question_activity(self, instance: Question, action, **kwargs):
         return dict(
             # data is overridden in model_observer
+            data=instance.survey,
             action='update',
             request_id='surveys',
             pk=instance.pk,
@@ -75,24 +80,24 @@ class SurveyConsumer(ListModelMixin, ObserverModelInstanceMixin, GenericAsyncAPI
 
     @model_observer(Choice)
     async def choice_activity(self, message, observer=None, action=None, **kwargs):
-        data = await self.get_choice_activity_data(pk=message['pk'])
-        if data is not None:
-            message['data'] = data
-            await self.send_json(message)
+        instance = message.pop('data')
+        message['data'] = await self.get_survey_serializer_data(survey=instance)
+        await self.send_json(message)
 
-    @database_sync_to_async
-    def get_choice_activity_data(self, pk):
-        choice_qs = Choice.objects.filter(pk=pk)
-        if not choice_qs.exists():
-            return None
-        survey = choice_qs.first().question.survey
-        serializer = SurveySerializer(instance=survey, context={'scope': self.scope})
-        return serializer.data
+    @choice_activity.groups_for_signal
+    def choice_activity(self, instance: Choice, **kwargs):
+        yield f'survey__{instance.question.survey.id}'
+
+    @choice_activity.groups_for_consumer
+    def choice_activity(self, survey=None, **kwargs):
+        if survey is not None:
+            yield f'survey__{survey}'
 
     @choice_activity.serializer
     def choice_activity(self, instance: Choice, action, **kwargs):
         return dict(
             # data is overridden in model_observer
+            data=instance.question.survey,
             action='update',
             request_id='surveys',
             pk=instance.pk,
@@ -100,10 +105,37 @@ class SurveyConsumer(ListModelMixin, ObserverModelInstanceMixin, GenericAsyncAPI
         )
 
     async def disconnect(self, code):
+        await self.unsubscribe()
+        await super().disconnect(code)
+
+    @action()
+    async def subscribe(self, pk, request_id, **kwargs):
+        await self.survey_activity.subscribe(pk=pk, request_id=request_id)
+        await self.question_activity.subscribe(survey=pk, request_id=request_id)
+        await self.choice_activity.subscribe(survey=pk, request_id=request_id)
+
+    async def unsubscribe(self):
         await self.survey_activity.unsubscribe()
         await self.question_activity.unsubscribe()
         await self.choice_activity.unsubscribe()
-        await super().disconnect(code)
+
+    @action()
+    async def list(self, request_id, page=1, page_size=20, **kwargs):
+        if page == 1:
+            await self.unsubscribe()
+        queryset, data = await self.list_(page, page_size, **kwargs)
+        pks = await database_sync_to_async(list)(queryset.values_list('id', flat=True))
+        for pk in pks:
+            await self.subscribe(pk=pk, request_id=request_id)
+        await self.reply(action='list', data=data, request_id=request_id)
+
+    @database_sync_to_async
+    def list_(self, page, page_size, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset(**kwargs), **kwargs)
+        page_obj = list_paginator(queryset, page, page_size)
+        serializer = SurveySerializer(page_obj.object_list, many=True, context={'scope': self.scope})
+        return page_obj.object_list, dict(results=serializer.data, current_page=page_obj.number,
+                                          has_next=page_obj.has_next())
 
     @action()
     async def create_response(self, data: dict, request_id: str, **kwargs):
