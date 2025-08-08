399680e7a8556e600ac54417ea5f57f7844c8f18
JamesMutahi <mutahijames0@gmail.com>
2025-08-08 10:41:41 +0300

    remove pagination on notifications

--- notification/consumers.py
+++ notification/consumers.py
@@ -2,6 +2,7 @@ from channels.db import database_sync_to_async
 from django.db.models import QuerySet
 from djangochannelsrestframework.decorators import action
 from djangochannelsrestframework.generics import GenericAsyncAPIConsumer
+from djangochannelsrestframework.mixins import ListModelMixin
 from djangochannelsrestframework.observer import model_observer
 
 from chat.utils.list_paginator import list_paginator
@@ -9,11 +10,10 @@ from notification.models import Notification
 from notification.serializers import NotificationSerializer
 
 
-class NotificationConsumer(GenericAsyncAPIConsumer):
+class NotificationConsumer(ListModelMixin, GenericAsyncAPIConsumer):
     serializer_class = NotificationSerializer
     queryset = Notification.objects.all()
     lookup_field = "pk"
-    page_size = 12
 
     def filter_queryset(self, queryset: QuerySet, **kwargs):
         queryset = super().filter_queryset(queryset=queryset, **kwargs)
@@ -54,22 +54,6 @@ class NotificationConsumer(GenericAsyncAPIConsumer):
             pk=instance.pk,
             response_status=201 if action.value == 'create' else 204 if action.value == 'delete' else 200
         )
-
-    @action()
-    async def list(self, request_id: str, last_notification: int = None, page_size=page_size, **kwargs):
-        notifications = self.filter_queryset(self.get_queryset(**kwargs), **kwargs)
-        data = await self.notifications_paginator(notifications=notifications, page_size=page_size,
-                                                  last_notification=last_notification, **kwargs)
-        return data, 200
-
-    @database_sync_to_async
-    def notifications_paginator(self, notifications, page_size, last_notification: int = None, **kwargs):
-        if last_notification:
-            notification = Notification.objects.get(pk=last_notification)
-            notifications = notifications.filter(id__lt=notification.id)
-        page_obj = list_paginator(queryset=notifications, page=1, page_size=page_size)
-        serializer = NotificationSerializer(page_obj.object_list, many=True, context={'scope': self.scope})
-        return dict(results=serializer.data, last_notification=last_notification, has_next=page_obj.has_next())
 
     @action()
     async def mark_as_read(self, pk: int, request_id, **kwargs):
