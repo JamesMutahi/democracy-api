210316de05374a0497bc6120f2724ce48521d9a1
JamesMutahi <mutahijames0@gmail.com>
2025-08-19 10:02:15 +0300

    change admin app layout

--- project/dashboard.py
+++ project/dashboard.py
@@ -5,54 +5,30 @@ from grappelli.dashboard import modules, Dashboard
 class CustomIndexDashboard(Dashboard):
     def __init__(self, **kwargs):
         Dashboard.__init__(self, **kwargs)
-        self.children.append(modules.AppList(
-            title=_(''),
+        # self.children.append(modules.AppList(
+        #     title=_(''),
+        #     column=1,
+        #     collapsible=True,
+        #     models=('django.contrib.sites.models.Site',),
+        # ))
+        self.children.append(modules.ModelList(
+            title=_('General'),
             column=1,
             collapsible=True,
-            models=('django.contrib.sites.models.Site',),
+            models=('poll.models.Poll', 'survey.models.Survey', 'users.models.CustomUser', 'posts.models.Report'),
         ))
-        self.children.append(modules.AppList(
-            title=_(''),
+        self.children.append(modules.ModelList(
+            title=_('User data'),
             column=1,
             collapsible=True,
-            models=('poll.models.Poll',),
-        ))
-        self.children.append(modules.AppList(
-            title=_(''),
-            column=1,
-            collapsible=True,
-            models=('users.models.CustomUser',),
-        ))
-        self.children.append(modules.AppList(
-            title=_(''),
-            column=1,
-            collapsible=True,
-            models=('posts.models.Post', 'posts.models.Report'),
-        ))
-        self.children.append(modules.AppList(
-            title=_(''),
-            column=1,
-            collapsible=True,
-            models=('chat.models.Chat',),
-        ))
-        self.children.append(modules.AppList(
-            title=_(''),
-            column=1,
-            collapsible=True,
-            models=('notification.models.Notification',),
-        ))
-        self.children.append(modules.AppList(
-            title=_(''),
-            column=1,
-            collapsible=True,
-            models=('survey.models.Survey', 'survey.models.Question', 'survey.models.Choice', 'survey.models.Response',
-                    'survey.models.TextAnswer', 'survey.models.ChoiceAnswer'),
+            models=(
+            'posts.models.Post', 'petition.models.Petition', 'chat.models.Chat', 'notification.models.Notification',),
         ))
         # append a recent actions module
         self.children.append(modules.RecentActions(
             title=_('Recent actions'),
             column=2,
-            collapsible=False,
+            collapsible=True,
             limit=5,
         ))
 
