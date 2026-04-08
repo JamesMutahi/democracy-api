8365b98919ebe218d5b80ce27baa7ca148927098
JamesMutahi <mutahijames0@gmail.com>
2026-04-08 11:44:07 +0300

    refactor meeting consumer

--- apps/meeting/consumers.py
+++ apps/meeting/consumers.py
@@ -2,12 +2,13 @@ from channels.db import database_sync_to_async
 from django.db.models import QuerySet, Q
 from djangochannelsrestframework.decorators import action
 from djangochannelsrestframework.generics import GenericAsyncAPIConsumer
-from djangochannelsrestframework.mixins import ListModelMixin, CreateModelMixin, PatchModelMixin, RetrieveModelMixin
+from djangochannelsrestframework.mixins import CreateModelMixin, ListModelMixin, PatchModelMixin, RetrieveModelMixin
 from djangochannelsrestframework.observer import model_observer
+from rest_framework.exceptions import PermissionDenied
 
-from apps.utils.list_paginator import list_paginator
 from apps.meeting.models import Meeting
 from apps.meeting.serializers import MeetingSerializer
+from apps.utils.list_paginator import list_paginator
 
 
 class MeetingConsumer(CreateModelMixin, ListModelMixin, PatchModelMixin, RetrieveModelMixin, GenericAsyncAPIConsumer):
@@ -22,139 +23,177 @@ class MeetingConsumer(CreateModelMixin, ListModelMixin, PatchModelMixin, Retriev
         else:
             await self.close()
 
+    # ====================== Real-time Observer ======================
     @model_observer(Meeting, many_to_many=True)
-    async def meeting_activity(self, message, observer=None, action=None, **kwargs):
-        pk = message['data']
+    async def meeting_activity(self, message, **kwargs):
         if message['action'] != 'delete':
-            message['data'] = await self.get_meeting_serializer_data(pk=pk)
+            message['data'] = await self.get_meeting_serializer_data(pk=message['data'])
         await self.send_json(message)
 
     @database_sync_to_async
     def get_meeting_serializer_data(self, pk: int):
-        meeting = Meeting.objects.get(pk=pk)
-        serializer = MeetingSerializer(instance=meeting, context={'scope': self.scope})
-        return serializer.data
+        meeting = Meeting.objects.select_related('host', 'county', 'constituency', 'ward').get(pk=pk)
+        return MeetingSerializer(meeting, context={'scope': self.scope}).data
 
     @meeting_activity.groups_for_signal
-    def meeting_activity(self, instance: Meeting, **kwargs):
+    def meeting_activity_groups(self, instance: Meeting, **kwargs):
         yield f'meeting__{instance.pk}'
 
     @meeting_activity.groups_for_consumer
-    def meeting_activity(self, pk=None, **kwargs):
+    def meeting_activity_groups(self, pk=None, **kwargs):
         if pk is not None:
             yield f'meeting__{pk}'
 
     @meeting_activity.serializer
-    def meeting_activity(self, instance: Meeting, action, **kwargs):
-        return dict(
-            # data is overridden in model_observer
-            # TODO: Too many database hits in model observer. Pass more fields to data in dict. Test with redis
-            data=instance.pk,
-            action=action.value,
-            request_id='meetings',
-            pk=instance.pk,
-            response_status=201 if action.value == 'create' else 204 if action.value == 'delete' else 200
-        )
+    def meeting_activity_serializer(self, instance: Meeting, action, **kwargs):
+        return {
+            'data': instance.pk,
+            'action': action.value,
+            'request_id': 'meetings',
+            'pk': instance.pk,
+            'response_status': 201 if action.value == 'create' else 204 if action.value == 'delete' else 200
+        }
 
     async def disconnect(self, code):
-        await self.meeting_activity.unsubscribe()
         await database_sync_to_async(self.scope['user'].listening_to.clear)()
+        await self.meeting_activity.unsubscribe()
         await super().disconnect(code)
 
+    # ====================== Filter with Permissions ======================
     def filter_queryset(self, queryset: QuerySet, **kwargs):
         queryset = super().filter_queryset(queryset=queryset, **kwargs)
-        previous_meetings = kwargs.get('previous_meetings', None)
+
+        previous_meetings = kwargs.get('previous_meetings')
+        action = kwargs.get('action')
+        search_term = kwargs.get('search_term')
+        is_active = kwargs.get('is_active', True)
+        filter_by_region = kwargs.get('filter_by_region', True)
+        sort_by = kwargs.get('sort_by', 'recent')
+        start_date = kwargs.get('start_date')
+        end_date = kwargs.get('end_date')
+        county = kwargs.get('county')
+        constituency = kwargs.get('constituency')
+        ward = kwargs.get('ward')
+
         if previous_meetings:
             queryset = queryset.exclude(id__in=previous_meetings)
-        if kwargs.get('action') == 'list':
-            search_term = kwargs.get('search_term', None)
-            is_active = kwargs.get('is_active', True)
-            filter_by_region = kwargs.get('filter_by_region', True)
-            sort_by = kwargs.get('sort_by', 'recent')
-            start_date = kwargs.get('start_date', None)
-            end_date = kwargs.get('end_date', None)
+
+        if action == 'list':
+            # Search
             if search_term:
-                queryset = queryset.filter(Q(title__icontains=search_term) | Q(description__icontains=search_term) | Q(
-                    host__name__icontains=search_term) | Q(county__name__icontains=search_term) | Q(
-                    constituency__name__icontains=search_term) | Q(ward__name__icontains=search_term)).distinct()
+                queryset = queryset.filter(
+                    Q(title__icontains=search_term) |
+                    Q(description__icontains=search_term) |
+                    Q(host__name__icontains=search_term) |
+                    Q(county__name__icontains=search_term) |
+                    Q(constituency__name__icontains=search_term) |
+                    Q(ward__name__icontains=search_term)
+                ).distinct()
+
+            # Active status
             if is_active is not None:
-                if is_active:
-                    queryset = queryset.filter(is_active=True)
-                if not is_active:
-                    queryset = queryset.filter(is_active=False)
-            if filter_by_region:
-                queryset = queryset.filter(Q(county__isnull=True) | Q(county=kwargs['county'])).filter(
-                    Q(constituency__isnull=True) | Q(constituency=kwargs['constituency'])).filter(
-                    Q(ward__isnull=True) | Q(ward=kwargs['ward']))
+                queryset = queryset.filter(is_active=bool(is_active))
+
+            # Regional filtering
+            if filter_by_region and (county or constituency or ward):
+                region_q = Q()
+                if county:
+                    region_q &= Q(county__isnull=True) | Q(county=county)
+                if constituency:
+                    region_q &= Q(constituency__isnull=True) | Q(constituency=constituency)
+                if ward:
+                    region_q &= Q(ward__isnull=True) | Q(ward=ward)
+                queryset = queryset.filter(region_q)
+
+            # Date range
             if start_date and end_date:
                 queryset = queryset.filter(start_time__range=(start_date, end_date))
-            if sort_by:
-                if sort_by == 'recent':
-                    return queryset.order_by('-start_time')
-                if sort_by == 'oldest':
-                    return queryset.order_by('start_time')
+
+            # Sorting
+            if sort_by == 'recent':
+                queryset = queryset.order_by('-start_time')
+            elif sort_by == 'oldest':
+                queryset = queryset.order_by('start_time')
+
             return queryset
-        if kwargs.get('action') == 'user_meetings':
+
+        elif action == 'user_meetings':
             return queryset.filter(host=kwargs.get('user'))
-        if kwargs.get('action') == 'delete' or kwargs.get('action') == 'patch':
-            return queryset.filter(host=self.scope['user'])
+
+        # === Permission Checks for Sensitive Actions ===
+        elif action in ['patch', 'delete']:
+            # Only the host can patch or delete a meeting
+            host_filter = Q(host=self.scope['user'])
+            return queryset.filter(host_filter)
+
         return queryset
 
+    # ====================== List & Create ======================
     @action()
-    async def create(self, data: dict, request_id: str, **kwargs):
-        response, status = await super().create(data, **kwargs)
-        await self.meeting_activity.subscribe(pk=response["id"], request_id=request_id)
-        return response, status
-
-    @action()
-    async def list(self, request_id, page_size=page_size, **kwargs):
+    async def list(self, request_id: str, page_size=None, **kwargs):
         kwargs['county'], kwargs['constituency'], kwargs['ward'] = await self.get_user_regions()
+
         queryset = self.filter_queryset(self.get_queryset(**kwargs), **kwargs)
-        data = await self.list_(queryset=queryset, page_size=page_size, **kwargs)
+        data = await self.list_(queryset=queryset, page_size=page_size or self.page_size, **kwargs)
+
         await self.reply(action='list', data=data, request_id=request_id)
 
     @database_sync_to_async
     def get_user_regions(self):
-        return self.scope['user'].county, self.scope['user'].constituency, self.scope['user'].ward
+        user = self.scope['user']
+        return user.county, user.constituency, user.ward
 
     @database_sync_to_async
-    def list_(self, queryset, page_size, **kwargs):
-        page_obj = list_paginator(queryset=queryset, page=1, page_size=page_size, )
-        serializer = MeetingSerializer(page_obj.object_list, many=True, context={'scope': self.scope})
-        return dict(results=serializer.data, previous_meetings=kwargs.get('previous_meetings', None),
-                    has_next=page_obj.has_next())
+    def list_(self, queryset, page_size: int, **kwargs):
+        page_obj = list_paginator(queryset=queryset, page=1, page_size=page_size)
+
+        serializer = MeetingSerializer(
+            page_obj.object_list,
+            many=True,
+            context={'scope': self.scope}
+        )
+
+        return {
+            'results': serializer.data,
+            'previous_meetings': kwargs.get('previous_meetings'),
+            'has_next': page_obj.has_next()
+        }
 
     @action()
-    async def user_meetings(self, request_id, page_size=page_size, **kwargs):
+    async def create(self, data: dict, request_id: str, **kwargs):
+        response, status = await super().create(data, **kwargs)
+        if isinstance(response, dict) and "id" in response:
+            await self.meeting_activity.subscribe(pk=response["id"], request_id=request_id)
+        return response, status
+
+    @action()
+    async def user_meetings(self, request_id: str, page_size=None, **kwargs):
         queryset = self.filter_queryset(self.get_queryset(**kwargs), **kwargs)
-        data = await self.list_(queryset=queryset, page_size=page_size, **kwargs)
+        data = await self.list_(queryset=queryset, page_size=page_size or self.page_size, **kwargs)
         return data, 200
 
+    # ====================== Join / Leave ======================
     @action()
-    async def join(self, pk, **kwargs):
-        meeting = await database_sync_to_async(self.get_object)(pk=pk)
-        in_region = await self.check_region(meeting=meeting)
-        if not in_region:
-            return await self.reply(action='join', errors=['You are not a registered voter in the region'], status=403)
-        data = await self.add_listener(meeting=meeting)
-        return data, 200
+    async def join(self, pk: int, **kwargs):
+        result = await self.perform_join(pk=pk)
+        if isinstance(result, dict) and result.get('error'):
+            return await self.reply(
+                action='join',
+                errors=[result['error']],
+                status=result.get('status', 403)
+            )
+        return result, 200
 
     @database_sync_to_async
-    def check_region(self, meeting: Meeting):
-        if not meeting.county:
-            return True
-        if meeting.county != self.scope['user'].county:
-            return False
-        if meeting.constituency:
-            if meeting.constituency != self.scope['user'].constituency:
-                return False
-        if meeting.ward:
-            if meeting.ward != self.scope['user'].ward:
-                return False
-        return True
+    def perform_join(self, pk: int):
+        try:
+            meeting = Meeting.objects.select_related('county', 'constituency', 'ward').get(pk=pk)
+        except Meeting.DoesNotExist:
+            return {'error': 'Meeting not found', 'status': 404}
 
-    @database_sync_to_async
-    def add_listener(self, meeting: Meeting):
+        if not self._user_can_access_meeting(meeting):
+            return {'error': 'You are not a registered voter in the region', 'status': 403}
+
         meeting.listeners.add(self.scope['user'])
         return MeetingSerializer(meeting, context={'scope': self.scope}).data
 
@@ -165,6 +204,55 @@ class MeetingConsumer(CreateModelMixin, ListModelMixin, PatchModelMixin, Retriev
         return {'pk': pk}, 200
 
     @database_sync_to_async
-    def remove_listener(self, pk):
-        meeting = self.get_object(pk=pk)
-        meeting.listeners.remove(self.scope['user'])
+    def remove_listener(self, pk: int):
+        try:
+            meeting = Meeting.objects.get(pk=pk)
+            meeting.listeners.remove(self.scope['user'])
+        except Meeting.DoesNotExist:
+            pass
+
+    def _user_can_access_meeting(self, meeting: Meeting) -> bool:
+        user = self.scope['user']
+        if not meeting.county:
+            return True
+        if meeting.county != user.county:
+            return False
+        if meeting.constituency and meeting.constituency != user.constituency:
+            return False
+        if meeting.ward and meeting.ward != user.ward:
+            return False
+        return True
+
+    # ====================== Retrieve & Subscription ======================
+    @action()
+    async def retrieve(self, request_id: str, **kwargs):
+        response, status = await super().retrieve(**kwargs)
+        if isinstance(response, dict):
+            pk = response.get("id")
+            if pk:
+                await self.meeting_activity.subscribe(pk=pk, request_id=request_id)
+        return response, status
+
+    # ====================== Permission-Protected Actions ======================
+    @action()
+    async def patch(self, pk: int, **kwargs):   # Override to add explicit check
+        meeting = await database_sync_to_async(self.get_object)(pk=pk)
+        if meeting.host_id != self.scope['user'].id:
+            return await self.reply(
+                action='patch',
+                errors=['Only the host can update this meeting'],
+                status=403
+            )
+        return await super().patch(**kwargs)
+
+    @action()
+    async def delete(self, pk: int, **kwargs):
+        meeting = await database_sync_to_async(self.get_object)(pk=pk)
+        if meeting.host_id != self.scope['user'].id:
+            return await self.reply(
+                action='delete',
+                errors=['Only the host can delete this meeting'],
+                status=403
+            )
+        response, status = await super().delete(**kwargs)
+        return response, status
