d2283f3d19ff0ef5f713bb762e5b0d7eaec47c72
JamesMutahi <mutahijames0@gmail.com>
2026-04-12 19:12:16 +0300

    remove unstable time decay to recommendations

--- apps/recommendations/follow_recommender.py
+++ apps/recommendations/follow_recommender.py
@@ -1,13 +1,11 @@
 import random
-from datetime import timedelta
 
 from django.core.cache import cache
-from django.db.models import Count, F, Value, FloatField, Case, When, Q, OuterRef, Subquery, ExpressionWrapper
-from django.db.models.functions import Now
+from django.db.models import Count, F, Value, FloatField, Case, When, Q, OuterRef, ExpressionWrapper
 from django.utils import timezone
 
-from apps.users.models import CustomUser, ProfileVisit
 from apps.posts.models import PostLike, PostClick
+from apps.users.models import CustomUser, ProfileVisit
 from .models import FollowRecommendationCache
 
 CACHE_TIMEOUT = 60 * 60  # 1 hour
@@ -39,22 +37,6 @@ class FollowRecommender:
             id=self.user.id
         ).exclude(id__in=excluded).exclude(id__in=muted).exclude(id__in=blocked)
 
-        # Subqueries for time decay
-        recent_visit_subquery = ProfileVisit.objects.filter(
-            visitor=self.user,
-            visited=OuterRef('pk')
-        ).order_by('-visited_at').values('visited_at')[:1]
-
-        recent_like_subquery = PostLike.objects.filter(
-            user=self.user,
-            post__author=OuterRef('pk')
-        ).order_by('-liked_at').values('liked_at')[:1]
-
-        recent_click_subquery = PostClick.objects.filter(
-            user=self.user,
-            post__author=OuterRef('pk')
-        ).order_by('-clicked_at').values('clicked_at')[:1]
-
         scored_users = base_qs.annotate(
             location_score=self._get_location_score(),
 
@@ -71,9 +53,25 @@ class FollowRecommender:
                 output_field=FloatField()
             ),
 
-            # Time-decayed signals
-            profile_visit_score=self._build_time_decay(recent_visit_subquery, max_days=30),
-            engagement_score=self._build_engagement_decay(recent_like_subquery, recent_click_subquery),
+            # Simple but effective time-decayed profile visit (stable version)
+            profile_visit_score=Case(
+                When(
+                    id__in=self.user.visits.values_list('id', flat=True),
+                    then=Value(0.85)   # Strong boost for visited profiles
+                ),
+                default=Value(0.0),
+                output_field=FloatField()
+            ),
+
+            # Simple engagement score (likes + clicks on this user's posts)
+            engagement_score=Case(
+                When(
+                    Q(liked_posts_through__user=self.user) | Q(clicked_posts_through__user=self.user),
+                    then=Value(0.70)
+                ),
+                default=Value(0.0),
+                output_field=FloatField()
+            ),
 
             activity_score=Count('posts', filter=Q(posts__status='published')),
         ).annotate(
@@ -81,7 +79,7 @@ class FollowRecommender:
                 F('location_score') * Value(0.30) +
                 F('mutual_score') * Value(0.35) +
                 F('profile_visit_score') * Value(0.15) +
-                F('engagement_score') * Value(0.15) +  # Time-decayed engagement
+                F('engagement_score') * Value(0.15) +
                 F('activity_score') * Value(0.05),
                 output_field=FloatField()
             )
@@ -102,58 +100,6 @@ class FollowRecommender:
 
         scored_list.sort(key=lambda u: getattr(u, 'final_score_with_jitter', 0), reverse=True)
         return scored_list[:limit]
-
-    # ====================== TIME DECAY HELPERS ======================
-
-    def _build_time_decay(self, timestamp_subquery, max_days=30):
-        """Time decay for profile visits"""
-        days_ago = ExpressionWrapper(
-            (Now() - Subquery(timestamp_subquery, output_field=timezone.datetime))
-            / timedelta(days=1),
-            output_field=FloatField()
-        )
-
-        return Case(
-            When(days_ago__isnull=False, then=Case(
-                When(days_ago__lte=3, then=Value(1.0)),
-                When(days_ago__lte=7, then=Value(0.80)),
-                When(days_ago__lte=14, then=Value(0.60)),
-                When(days_ago__lte=max_days, then=Value(0.35)),
-                default=Value(0.15),
-                output_field=FloatField()
-            )),
-            default=Value(0.0),
-            output_field=FloatField()
-        )
-
-    def _build_engagement_decay(self, recent_like_subq, recent_click_subq):
-        """Time decay for engagement (likes + clicks) on this user's posts"""
-        days_since_like = ExpressionWrapper(
-            (Now() - Subquery(recent_like_subq, output_field=timezone.datetime))
-            / timedelta(days=1),
-            output_field=FloatField()
-        )
-        days_since_click = ExpressionWrapper(
-            (Now() - Subquery(recent_click_subq, output_field=timezone.datetime))
-            / timedelta(days=1),
-            output_field=FloatField()
-        )
-
-        return Case(
-            # If user has liked or clicked this author's posts recently
-            When(
-                Q(days_since_like__isnull=False) | Q(days_since_click__isnull=False),
-                then=Case(
-                    When(days_since_like__lte=7, then=Value(0.90)),      # Recent like
-                    When(days_since_click__lte=7, then=Value(0.85)),     # Recent click
-                    When(days_since_like__lte=30, then=Value(0.60)),
-                    default=Value(0.30),
-                    output_field=FloatField()
-                )
-            ),
-            default=Value(0.0),
-            output_field=FloatField()
-        )
 
     def _get_location_score(self):
         if not self.user.county:
